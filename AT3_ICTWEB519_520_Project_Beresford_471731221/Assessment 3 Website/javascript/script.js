b287d9f891fd5a1b4838efff1535b5250c3b89a6
Berriano <beresford2102@yahoo.com.au>
2023-05-18 12:57:23 +1000

    nearFinal

--- AT3_ICTWEB519_520_Project_Beresford_471731221/Assessment 3 Website/javascript/script.js
+++ AT3_ICTWEB519_520_Project_Beresford_471731221/Assessment 3 Website/javascript/script.js
@@ -1,27 +1,55 @@
-const btn = document.querySelector(".btn");
+// MENU
+
+
+const btn = document.querySelector(".main_btn");
 
 const menu = document.querySelector(".hidden-menu");
+
+const hamburger = document.querySelector(".icon");
+
+
+const accordion = document.querySelectorAll('.accordion__grid');
+
+const chevron = document.querySelectorAll('.accordion__icon-chevron');
+
+
 
 
 btn.addEventListener('click', function () {
 
 	menu.classList.toggle('hidden');
+	hamburger.classList.toggle('burger-open');
 
-})
-
-
-
-let acc = document.getElementsByClassName('accordion__grid');
-
-
-
-
-for (let i = 0; i < acc.length; i++) {
-
-	acc[i].addEventListener('click', function () {
-		this.classList.toggle('open');
-
-	})
 }
 
+)
 
+
+// ACCORDION
+
+
+
+
+
+
+
+
+
+accordion.forEach(acc => acc.addEventListener('click', function () {
+
+	this.classList.toggle('open');
+
+}
+
+))
+
+
+
+
+chevron.forEach(chev => chev.addEventListener('click', function () {
+
+	this.classList.toggle('rotate');
+
+}
+
+))
