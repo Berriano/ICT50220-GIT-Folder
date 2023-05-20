b5ad1390e57342885f9107b3d9c911cf19066c9e
Berriano <beresford2102@yahoo.com.au>
2023-05-20 14:09:46 +1000

    modal-video

--- AT3_ICTWEB519_520_Project_Beresford_471731221/Assessment 3 Website/javascript/script.js
+++ AT3_ICTWEB519_520_Project_Beresford_471731221/Assessment 3 Website/javascript/script.js
@@ -1,37 +1,35 @@
 // MENU
 
 
-const btn = document.querySelector(".main_btn");
+// const divHead = document.querySelectorAll('.div-head');
+// const btn = document.querySelector(".nav-btn");
+// const menu = document.querySelector(".hidden-menu");
+// const hamburger = document.querySelector(".hamburger-icon");
 
-const menu = document.querySelector(".hidden-menu");
 
-const hamburger = document.querySelector(".icon");
+// // needed for import
+
+// btn.addEventListener('click', function () {
+
+// 	menu.classList.toggle('hidden');
+// 	hamburger.classList.toggle('burger-open');
+
+// }
+
+// )
+
+// console.log(divHead, btn, menu, hamburger);
+
+
+
+
+
+
+/// ACCORDION ///
 
 
 const accordion = document.querySelectorAll('.accordion__grid');
-
-const chevron = document.querySelectorAll('.accordion__icon-chevron');
-
-
-
-
-btn.addEventListener('click', function () {
-
-	menu.classList.toggle('hidden');
-	hamburger.classList.toggle('burger-open');
-
-}
-
-)
-
-
-// ACCORDION
-
-
-
-
-
-
+const chevron = document.querySelectorAll('.chevron');
 
 
 
@@ -43,9 +41,6 @@ accordion.forEach(acc => acc.addEventListener('click', function () {
 
 ))
 
-
-
-
 chevron.forEach(chev => chev.addEventListener('click', function () {
 
 	this.classList.toggle('rotate');
@@ -53,3 +48,36 @@ chevron.forEach(chev => chev.addEventListener('click', function () {
 }
 
 ))
+
+
+/// video
+
+const modal = document.querySelector('.modal');
+const video = document.querySelector('.video');
+const formBtn = document.querySelector('.form__btn--help');
+const closeBtn = document.getElementById('close-btn');
+const modalCode = document.querySelector('.modal').innerHTML;
+
+console.log(closeBtn);
+
+
+formBtn.addEventListener('mouseover', function () {
+
+	modal.classList.add('show-modal');
+
+	modal.innerHTML = "<iframe	src='https://www.youtube.com/embed/y881t8ilMyc?autoplay=1&mute=1'	title='Help Video' class='video ' height='500px' width='750px' name='youtube' noborder='0' width='830' height='800' scrolling='yes' seamless></iframe>"
+
+
+})
+
+
+
+closeBtn.addEventListener('click', function () {
+
+	modal.classList.remove('show-modal');
+
+	modal.innerHTML = "<iframe	src='https://www.youtube.com/embed/y881t8ilMyc'	title='Help Video' class='video ' height='500px' width='750px' name='youtube' noborder='0' width='830' height='800' scrolling='yes' seamless></iframe>"
+
+
+
+})
